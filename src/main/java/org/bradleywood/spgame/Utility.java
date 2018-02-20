c8dfbacddc23c638eb0237221811e986ccf76ff9
BradleyWood <bradley.wood@uoit.net>
2018-02-20 17:03:45 -0500

    more utility functions

--- src/main/java/org/bradleywood/spgame/Utility.java
+++ src/main/java/org/bradleywood/spgame/Utility.java
@@ -6,7 +6,13 @@ import java.util.List;
 
 public class Utility {
 
-    public static List<BufferedImage> imageSplit(final BufferedImage image, int d) {
+    /**
+     * Split and image into d by d sub images
+     * @param image The original image
+     * @param d The dimensions
+     * @return The list of (d by d) sub images
+     */
+    public static List<BufferedImage> imageSplit(final BufferedImage image, final int d) {
         List<BufferedImage> images = new ArrayList<>();
         int width = image.getWidth() / d;
         int height = image.getHeight() / d;
@@ -18,5 +24,68 @@ public class Utility {
         }
 
         return images;
+    }
+
+    /**
+     * Weather two tiles directly touch
+     * @param idxA The first tile index
+     * @param idxB The second tile index
+     * @param sideLen The number tiles on each side
+     * @return
+     */
+    public static boolean isTouching(final int idxA, final int idxB, final int sideLen) {
+        int x1 = getX(idxA, sideLen);
+        int y1 = getY(idxA, sideLen);
+        int x2 = getX(idxB, sideLen);
+        int y2 = getY(idxB, sideLen);
+        return Math.ceil(Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2))) <= 1;
+    }
+
+    /**
+     * Find what tile (x or y) coordinate is represented by the location on screen
+     * @param screenCoord The (x or y) position in pixels
+     * @param length The width or height of game image
+     * @return The tile coord
+     */
+    public static int screenToTileCoord(final int screenCoord, final int length, final int sideLen) {
+        return sideLen * screenCoord / length;
+    }
+
+    public static int screenToTileIndex(final int xCoord, final int yCoord, final int width,
+                                        final int height, final int sideLen) {
+        return getIdx(screenToTileCoord(xCoord, width, sideLen), screenToTileCoord(yCoord, height, sideLen), sideLen);
+    }
+
+    /**
+     * Calculates the tile index based on its coordinates
+     *
+     * @param x X tile coordinate
+     * @param y Y tile coordinate
+     * @param sideLen The number of tiles on each side
+     * @return The tile index
+     */
+    public static int getIdx(final int x, final int y, final int sideLen) {
+        return y * sideLen + x;
+    }
+
+
+    /**
+     * Calculate the tile X coordinate given its tile index
+     * @param idx The tile index
+     * @param sideLen The number of tiles on each side
+     * @return The x coord
+     */
+    public static int getX(final int idx, final int sideLen) {
+        return idx % sideLen;
+    }
+
+    /**
+     * Calculate the tile Y coordinate given its tile index
+     * @param idx The tile index
+     * @param sideLen The number of tiles on each side
+     * @return The y coord
+     */
+    public static int getY(final int idx, final int sideLen) {
+        return idx / sideLen;
     }
 }
