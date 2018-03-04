9676dca5b3435e375e0364c629d7f8aa85ef925e
BradleyWood <bradley.wood@uoit.net>
2018-03-03 19:57:50 -0500

    added resources; maven jar assembly;

--- src/main/java/org/bradleywood/spgame/Application.java
+++ src/main/java/org/bradleywood/spgame/Application.java
@@ -3,7 +3,6 @@ package org.bradleywood.spgame;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 
 public class Application {
@@ -12,7 +11,7 @@ public class Application {
         JFrame frame = new JFrame("Sliding Puzzle");
 
         try {
-            BufferedImage image = ImageIO.read(new File("steam.png"));
+            BufferedImage image = ImageIO.read(Application.class.getClassLoader().getResourceAsStream("numbers.png"));
             GameModel model = new GameModel(Utility.imageSplit(image, 4), image.getWidth(), image.getHeight());
             GameView view = new GameView(model);
             Game game = new Game(view, model);
