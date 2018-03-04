d60d9ac8967bb58f7f35e48b561e1f2e0a629082
BradleyWood <bradley.wood@uoit.net>
2018-03-03 19:36:54 -0500

    test puzzle randomization

---
+++ src/test/java/org/bradleywood/spgame/GameModelTest.java
@@ -0,0 +1,63 @@
+package org.bradleywood.spgame;
+
+import org.sqtf.annotations.Before;
+import org.sqtf.annotations.Test;
+import org.sqtf.assertions.Assert;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class GameModelTest {
+
+    private BufferedImage image;
+    private List<BufferedImage> images;
+
+    @Before
+    public void setupImages() throws IOException {
+        image = ImageIO.read(new File("numbers.png"));
+        images = Utility.imageSplit(image, 4);
+    }
+
+    @Test
+    public void testRandomize() {
+        GameModel model = new GameModel(images, image.getWidth(), image.getHeight());
+
+        for (int i = 0; i < 200; i++) {
+            model.randomize();
+
+            int[] piecePositions = model.getPiecePositions();
+
+            Assert.assertEquals(images.size(), piecePositions.length);
+
+            // check that puzzle contains all unique pieces (no duplicates)
+            int[] count = new int[model.getPiecePositions().length];
+
+            for (int j = 0; j < piecePositions.length; j++) {
+                count[piecePositions[j]]++;
+            }
+
+            Assert.assertTrue(Arrays.stream(count).filter(c -> c == 1).count() == count.length);
+
+            // puzzle is solvable if the number of inversions is even
+            boolean validPuzzle = countInversions(piecePositions) % 2 == 0;
+            Assert.assertTrue(validPuzzle);
+
+        }
+    }
+
+    private int countInversions(int[] array) {
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
